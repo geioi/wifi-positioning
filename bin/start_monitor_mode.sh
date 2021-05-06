42c1c742d3cde1c37b667e221da070c29f82d122
Geio <geio96@hotmail.com>
2021-05-06 13:20:09 +0300

    Fixed typo

--- bin/start_monitor_mode.sh
+++ bin/start_monitor_mode.sh
@@ -3,5 +3,5 @@ if [ -n "$1" ]; then
 	sudo ifconfig mon0 up
 	echo "interface mon0 based on $1 is now running in monitor mode!"
 else
-	echo "Missing interface parameter! You can check the available interfaces by typing 'ipconfig'"
+	echo "Missing interface parameter! You can check the available interfaces by typing 'ifconfig'"
 fi
